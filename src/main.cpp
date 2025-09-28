395166e4070b4003af329e5ae1b94ff6c7687206
Konrad Wojas <github@m.wojas.nl>
2025-09-28 23:23:27 +0800

    Basic keypad support

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
 #include "motor.h"
 
@@ -79,6 +80,18 @@ void mqtt_reconnect() {
       Serial.println(" try again later");
     }
 }
+
+#define KP_ROW_NUM     4 // four rows
+#define KP_COLUMN_NUM  4 // four columns
+char keys[KP_ROW_NUM][KP_COLUMN_NUM] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte pin_rows[KP_ROW_NUM]      = {15, 2, 0, 4};
+byte pin_column[KP_COLUMN_NUM] = {16, 17, 5, 18};
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, KP_ROW_NUM, KP_COLUMN_NUM );
 
 
 // FIXME: What are these params?
@@ -258,6 +271,13 @@ void loop() {
     last_lcd = now;
   }
 
+  char key = keypad.getKey();
+  if (key) {
+    Serial.printf("Keypad: %c\n", key);
+    lcd.setCursor(15, 1);
+    lcd.write(key);
+  }
+
   //Serial.println("loop done");
-  delay(100);
+  delay(10);
 }
